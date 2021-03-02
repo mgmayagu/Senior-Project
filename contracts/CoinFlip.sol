f50b57660f6746fc2a3fcdccb4fea86ccc1e62bb
dovbrand <75651661+dovbrand@users.noreply.github.com>
2021-03-02 12:40:58 -0500

    Added forfeit, deposit, & annotations
    Added forfeit game function, casino deposit function, and annotations

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -8,42 +8,59 @@ contract CoinFlip {
     
     struct Player {
         address payable playerAddress;
-        uint256 betAmount;
-        uint256 gameTimeOut;
-        bytes32 commitHash;
-        bool win;
-        uint256 result;
+        bool playerExists; // Whether the player has already been added to map and array;
+        uint256 betAmount; // Bet Amount for the game
+        uint256 gameTimeOut; // How much time is left in the game
+        bytes32 commitHash; // Player choice hash
+        bool win; // Whether the player won or lost
+        uint256 result; // Result of the coinFlip for the player
+        bool gameInProgress; // Whether the player is currently playing a game
     }
     
-    mapping(address => Player) playerMap;
-    address[] playerAddressArray;
-    address payable casino;
-    uint256 public casinoDeposit;
-    uint256 maxBet = .001 ether;
-    address payable contractAddress = address(this);
+    mapping(address => Player) playerMap; // Map containing all players playing the game (this is not iterable)
+    address[] playerAddressArray; // Array of all players addresses of players playing the game (this is iterable)
+    address payable casino;  // Address of the casino, will not change after contract is deployed
+    uint256 public casinoDeposit = 0; // Value of the casino deposit
+    uint256 maxBet = .001 ether; 
+    address payable contractAddress = address(this);  // Address of this contract
     
     constructor () public payable{
-        // Casino initiates the contract by depositing 1 ether
-        require(msg.value == 1 ether);
-        casinoDeposit = msg.value;
+        // Casino initiates the contract
         casino = msg.sender;
     }
     
+    // Function for the casino to deposit money, must be called for the rest of the contract to work
+    function depositMoney() public payable {
+        require(msg.sender == casino, "Only the casino can deposit money");
+        casinoDeposit = casinoDeposit + msg.value;
+    }
     
+    // Function to submit bet, input the hash and a value to bet
     function playerCommit(bytes32 _commitHash) external payable {
-        require(msg.value < casinoDeposit);
-        require(msg.value >= 1 wei, "Bets must be  at least 1 wei");
-        require(msg.value <= maxBet, "Max bet exceeded");
         address _playerAddress  = msg.sender;
-        playerAddressArray.push(msg.sender);
-        playerMap[_playerAddress].playerAddress = msg.sender;
+        require(_playerAddress != casino, "Casino cannot be a player");
+        require(msg.value < casinoDeposit, "Casino cannot cover bet"); // Bet must be less than the money in the casino deposit to ensure casino can cover the bet
+        require(msg.value >= 1 wei, "Bets must be  at least 1 wei"); // Must be greater or equal to minimum bet of 1 wei
+        require(msg.value <= maxBet, "Max bet exceeded"); // Must be less than or equal to max bet of .001 ether
+        
+        if( !playerMap[_playerAddress].playerExists) { // If player is new
+            playerAddressArray.push(msg.sender); // Add player address to array of player addresses
+            playerMap[_playerAddress].playerAddress = msg.sender; 
+            playerMap[_playerAddress].playerExists = true;
+        }
+        else {
+            require( playerMap[_playerAddress].gameInProgress == false, "Player cannot be playing a game");
+        }
+    
+        // playerMap[_playerAddress] means the Player who is mapped to the address of _playerAddress
+        // Assign values to the Player (based on the Player struct)
         playerMap[_playerAddress].betAmount = msg.value;
+        playerMap[_playerAddress].gameInProgress = true;
         playerMap[_playerAddress].commitHash = _commitHash;
-        sendViaCall(contractAddress, playerMap[_playerAddress].betAmount);
-        matchBet(playerMap[_playerAddress].betAmount);
-        playerMap[_playerAddress].gameTimeOut = now + 2 minutes;
-        // timer(gameTimeOut);
-        playerMap[_playerAddress].result = flipCoin();
+        sendViaCall(contractAddress, playerMap[_playerAddress].betAmount); // Sends bet to the contract
+        matchBet(playerMap[_playerAddress].betAmount); // Contract will match the bet
+        playerMap[_playerAddress].gameTimeOut = now + 2 minutes; // Player will have 2 minutes from when they place the bet to claim their winnings
+        playerMap[_playerAddress].result = flipCoin(); // Stores the result of the coin flip for that player
     }
     
     // Function to receive Ether. msg.data must be empty
@@ -52,45 +69,53 @@ contract CoinFlip {
     // Fallback function is called when msg.data is not empty
     fallback() external payable {}
     
+    // Function to send a value of money to an address
     function sendViaCall(address payable _to, uint256 _amount) internal {
-        (bool sent, bytes memory data) = _to.call{value: _amount}("");
+        (bool sent, bytes memory data) = _to.call{value: _amount}(""); 
         require(sent, "Failed to send Ether");
     }
     
+    // Funtion to return total balance of the contract which is casinoDeposit + all bets currently on the table
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
     
-    function flipCoin() internal returns (uint256) {
+    // Function to generate pseudorandom number 1 or 0
+    function flipCoin() view internal returns (uint256) {
         return uint(keccak256(abi.encodePacked(now))) % 2; 
     }
     
+    // Function to see result of the coin flip
     function seeResult() public view returns(uint){
-        require (playerMap[msg.sender].betAmount > 0, "Please place your bet" );
+        // Can only be called once bet is placed (coin will automatically be flipped upon bet)
+        require (playerMap[msg.sender].betAmount > 0, "Please place your bet" ); 
         return playerMap[msg.sender].result;
     }
     
+    // Function for the casino to match the bet amount
     function matchBet(uint256 _betAmount) internal {
-        casinoDeposit = casinoDeposit - _betAmount;
+        casinoDeposit = casinoDeposit - _betAmount; // bet amount is deducted from casinoDeposit and but remains in the contract balance
     }
     
+    // Function to pay the winner
     function payOut(address payable _playerAddress) internal{
-        if (playerMap[_playerAddress].win == true){
-            sendViaCall(_playerAddress, 2 * playerMap[_playerAddress].betAmount);
-            playerMap[_playerAddress].betAmount = 0;
-            playerMap[_playerAddress].gameTimeOut = 0;
+        if (playerMap[_playerAddress].win == true && playerMap[_playerAddress].gameTimeOut > now ){ // If the player wins and is within time limit
+            sendViaCall(_playerAddress, 2 * playerMap[_playerAddress].betAmount); // Send money to player
         }
-        else {
-            casinoDeposit = casinoDeposit + 2 * playerMap[_playerAddress].betAmount;
-            playerMap[_playerAddress].betAmount = 0;
+        else { // If the casino wins
+            casinoDeposit = casinoDeposit + 2 * playerMap[_playerAddress].betAmount; // Send money to casino deposit
         }
+        playerMap[_playerAddress].betAmount = 0; // reset bet
+        playerMap[_playerAddress].gameTimeOut = 0; // reset game timer
+        playerMap[_playerAddress].gameInProgress = false; // game is over
     }
     
+    // Funtion to reveal player choice, input choice and hash value used to create initial hash
     function reveal(uint  _choice, bytes32 _hashValue) public payable {
-        require(now < playerMap[msg.sender].gameTimeOut, "Game Timed Out");
-        require (playerMap[msg.sender].betAmount > 0, "Please place your bet" );
-        // require (getBalance() == 2 * betAmount, "Please wait for the casino to match your bet" );
-        require(keccak256(abi.encodePacked(_choice, _hashValue)) == playerMap[msg.sender].commitHash, "Commitment hash does not match");
+        require(now < playerMap[msg.sender].gameTimeOut, "Game Timed Out"); // Can only reveal if the game is still valid
+        require (playerMap[msg.sender].betAmount > 0, "Please place your bet" ); // Must place a bet before player can reveal
+        // Hashed choice and hash must match initial hash
+        require(keccak256(abi.encodePacked(_choice, _hashValue)) == playerMap[msg.sender].commitHash, "Commitment hash does not match"); 
         if(_choice == playerMap[msg.sender].result ){
             playerMap[msg.sender].win = true;
         }
@@ -100,15 +125,24 @@ contract CoinFlip {
         payOut(msg.sender);
     }
     
+    // function to forfeit game instead of waiting for timer to end and instead of revealing
+    function  forfeitGame() public {
+        address _playerAddress = msg.sender;
+        require (playerMap[_playerAddress].playerAddress == _playerAddress); // Ensures that a player is calling the funtion
+        require (playerMap[_playerAddress].gameInProgress == true); // Game must be in progress
+        casinoDeposit = casinoDeposit + 2* playerMap[_playerAddress].betAmount; // Pays the money to the casino deposit
+        playerMap[_playerAddress].betAmount = 0;
+        playerMap[_playerAddress].gameTimeOut = 0;
+        playerMap[_playerAddress].gameInProgress = false;
+    }
     
-    
+    // Function to pay casino when time is up
     function collectGameTimeout() public {
-        for(uint256 i = 0; i < playerAddressArray.length; i++){
-            if (playerMap[playerAddressArray[i]].gameTimeOut <  now){
-                casinoDeposit = casinoDeposit + 2* playerMap[playerAddressArray[i]].betAmount;
-                playerMap[playerAddressArray[i]].betAmount = 0;
-                playerMap[playerAddressArray[i]].gameTimeOut = 0;
-            }
-        }
+        address _playerAddress = msg.sender;
+        require(playerMap[_playerAddress].gameTimeOut <  now, "game still in progress");
+        casinoDeposit = casinoDeposit + 2* playerMap[_playerAddress].betAmount;
+        playerMap[_playerAddress].betAmount = 0;
+        playerMap[_playerAddress].gameTimeOut = 0;
+        playerMap[_playerAddress].gameInProgress = false; 
     }
 }
