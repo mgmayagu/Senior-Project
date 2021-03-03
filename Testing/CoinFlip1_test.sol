01ab4f2d6e6e4f104ad6ce82a82544cfd759437b
hpdnnr7 <hpdnnr7@gmail.com>
2021-03-02 23:22:16 -0500

    Update CoinFlip1_test.sol

--- Testing/CoinFlip1_test.sol
+++ Testing/CoinFlip1_test.sol
@@ -7,10 +7,11 @@ import "../github/mgmayagu/Senior-Project/contracts/CoinFlip.sol";
 
 
 contract CoinFlipTest is CoinFlip {
+    // create a new instance of Value contract
     //CoinFlip cp;
     
     
-    //Define variables referring to different accounts
+    //Define variables where address 0 is the casino and address 1 serves as the player 1
     address acc0; // casino
     address acc1; // player 1
 
@@ -23,14 +24,15 @@ contract CoinFlipTest is CoinFlip {
     
 
     // Account 0 creates a new contract.
-    // Tests if initialized balance is correct and the casino address.
+    /// Test if initial owner is set correctly
+    // account at zero index (account-0) is default account, so current owner should be acc0
     /// #sender: account-0
     /// #value: 10
     function testContractCreated() public payable {
         Assert.equal(msg.sender, acc0, 'acc0 should be the sender of this function check');
         Assert.equal(msg.value, 10, '10 should be the value of this function check');
         contractCreated();
-        Assert.equal(getBalance() , msg.value, 'balance of contract should be 5 ');
+        Assert.equal(getBalance() , msg.value, 'balance of contract should be 10 ');
         Assert.equal(acc0, msg.sender, 'casino should be set to acc0');
     }
     
@@ -38,7 +40,7 @@ contract CoinFlipTest is CoinFlip {
     // Tests if player choice is commited correctly and if the balance of the contract increased.
     /// #sender: account-1
     /// #value: 2
-    function testPlayer1Commit() public payable {
+    function testPlayer1Commitment() public payable {
         Assert.equal(msg.sender, acc1, 'acc1 should be the sender of this function check');
         Assert.equal(msg.value, 2, '2 should be the value of this function check');
         uint init_balance = getBalance();
