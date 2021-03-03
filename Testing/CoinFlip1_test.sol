cc3d173a5d1e1bebf8d4c113cdc4826d10319aa3
hpdnnr7 <hpdnnr7@gmail.com>
2021-03-02 22:03:43 -0500

    Update CoinFlip1_test.sol

--- Testing/CoinFlip1_test.sol
+++ Testing/CoinFlip1_test.sol
@@ -7,63 +7,56 @@ import "../github/mgmayagu/Senior-Project/contracts/CoinFlip.sol";
 
 
 contract CoinFlipTest is CoinFlip {
-    CoinFlip cp;
+    //CoinFlip cp;
     
     
     //Define variables referring to different accounts
     address acc0; // casino
     address acc1; // player 1
-    address acc2; // malicous 2nd player
 
     
     function beforeAll () public returns (bool){
-        // Create an instance of contract to be tested
-        cp = new CoinFlip();
         
         acc0 = TestsAccounts.getAccount(0); 
         acc1 = TestsAccounts.getAccount(1);
-        acc2 = TestsAccounts.getAccount(2);
     }
     
 
     // Account 0 creates a new contract.
     // Tests if initialized balance is correct and the casino address.
     /// #sender: account-0
-    /// #value: 5
+    /// #value: 10
     function testContractCreated() public payable {
-        Assert.equal(msg.sender, casino, 'acc0 should be the sender of this function check');
-        Assert.equal(msg.value, 5, '2 should be the value of this function check');
-        sendViaCall(msg.sender, msg.value);
-        Assert.equal(getBalance(), msg.value, 'balance of contract should be 2 (how much casino put in)');
-        Assert.equal(casino, msg.sender, 'casino should be set to acc0');
+        Assert.equal(msg.sender, acc0, 'acc0 should be the sender of this function check');
+        Assert.equal(msg.value, 10, '10 should be the value of this function check');
+        contractCreated();
+        Assert.equal(getBalance() , msg.value, 'balance of contract should be 5 ');
+        Assert.equal(acc0, msg.sender, 'casino should be set to acc0');
     }
     
     // Account 1 starts a game with 1 wei and submits a choice of 0.
     // Tests if player choice is commited correctly and if the balance of the contract increased.
     /// #sender: account-1
-    /// #value: 1
+    /// #value: 2
     function testPlayer1Commit() public payable {
-        //Assert.equal(msg.sender, cp.Player, 'acc1 should be the sender of this function check');
-         Assert.equal(msg.value, 1, '1 should be the value of this function check');
-         uint init_balance = getBalance();
-         //playerCommit(0);
-        // int gameIdx = get_game_idx(msg.sender);
-        // Assert.equal(gameIdx, 0, 'Account 1 should be a player in a game now');
-        // Assert.equal(get_balance(), init_balance + 1, 'Balance of contract should be 6 (5 initial from casino + 1 from player');
-        // Assert.equal(Player[uint(gameIdx)].player_choice, 0, 'Player 1 choice should be 0');
-    }
-    
-    function testPlayer2Commit() public payable {
-    //     // Assert.equal(msg.sender, acc2, 'acc2 should be the sender of this function check');
-        Assert.equal(msg.value, 1, '1 should be the value of this function check');
+        Assert.equal(msg.sender, acc1, 'acc1 should be the sender of this function check');
+        Assert.equal(msg.value, 2, '2 should be the value of this function check');
         uint init_balance = getBalance();
-    //     // player_commit(1);
-    //     // int gameIdx = get_game_idx(msg.sender);
-    //     // Assert.equal(gameIdx, 1, 'Account 2 should be a player in a game now');
-    //     Assert.equal(getBalance(), init_balance + 1, 'Balance of contract should be 7 (6 current + 1 from acc2');
-    //     // Assert.equal(Games[uint(gameIdx)].player_choice, 1, 'Player 2 choice should be 1');
+        Assert.equal(getBalance(), init_balance, 'Balance of contract should be 12 (10 initial from casino + 2 from player');
+        Assert.equal(acc1, msg.sender, 'player 1 should be set to acc1');
     }
+    
+    // Balance should be 5 (initial) + 2 (player1 and player2) = 7 total
+    function checkBalance() public {
+        Assert.equal(getBalance(), 12, "Balance should be 10 (initial) + 2 (player1 and player2) = 7");
+    }
+    
+    function checkSuccess() public pure returns (bool) {
+        // Use the return value (true or false) to test the contract
+        return true;
+    }
+    
 
     
-}
 
+}
